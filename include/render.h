0345f9e1fabe1344827c98ef0ecd78bb69f91377
stev0025 <alex.stevenloekita@gmail.com>
2023-10-12 17:36:28 +0800

    initialize render

---
+++ include/render.h
@@ -0,0 +1,14 @@
+/**
+ * @brief render the whole display
+ */
+void render_overall_display();
+
+/**
+ * @brief render the game board
+ */
+void render_gameboard();
+
+/**
+ * @brief render the ruler
+ */
+void render_gameboard();
