c48dfeffc12b778eeb84f59d1742d5f2a197f9a9
Steven <alex.stevenloekita@gmail.com>
2023-10-26 12:25:26 +0700

    create file: event

--- include/state_machine.h
+++ include/state_machine.h
@@ -1,11 +1,12 @@
 /* list of states */
-enum StateMachineStates {
+typedef enum {
+  STATE_NONE,
   STATE_INIT,
   STATE_PLAYER_X_TURN,
   STATE_PLAYER_O_TURN,
   STATE_CHECK_WIN,
   STATE_END_GAME
-};
+} StateMachineStates;
 
 /**
  * @brief transitions from current to new states
